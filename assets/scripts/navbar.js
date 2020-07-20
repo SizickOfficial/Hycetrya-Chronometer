f709d77f347e653660f79da8eeaeb9bbeddd9b97
SizickOfficial <sizick@leviath.fr>
2020-07-21 01:45:36 +0200

    Ajout d'une navbar, ajout d'autres features

---
+++ assets/scripts/navbar.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+    $("#skyswordButton").click(function () {
+        document.getElementById('skysword-content').style.display = 'flex';
+        document.getElementById('skydaria-olympe-content').style.display = 'none';
+        document.getElementById('skydaria-pirate-content').style.display = 'none';
+        document.getElementById('hadaria-content').style.display = 'none';
+    });
+
+    $("#skydariaOlympeButton").click(function () {
+        document.getElementById('skysword-content').style.display = 'none';
+        document.getElementById('skydaria-olympe-content').style.display = 'flex';
+        document.getElementById('skydaria-pirate-content').style.display = 'none';
+        document.getElementById('hadaria-content').style.display = 'none';
+    });
+
+    $("#skydariaPirateButton").click(function () {
+        document.getElementById('skysword-content').style.display = 'none';
+        document.getElementById('skydaria-olympe-content').style.display = 'none';
+        document.getElementById('skydaria-pirate-content').style.display = 'flex';
+        document.getElementById('hadaria-content').style.display = 'none';
+    });
+
+    $("#hadariaButton").click(function () {
+        document.getElementById('skysword-content').style.display = 'none';
+        document.getElementById('skydaria-olympe-content').style.display = 'none';
+        document.getElementById('skydaria-pirate-content').style.display = 'none';
+        document.getElementById('hadaria-content').style.display = 'flex';
+    });
+});
